78682d5ed17dcbabbda2029ee42b150bcac93182
David Ryan <0aveRyan@users.noreply.github.com>
2018-01-10 12:13:26 -0700

    Update wordpress-development-toolkit.php

--- wordpress-development-toolkit.php
+++ wordpress-development-toolkit.php
@@ -16,7 +16,9 @@ if ( ! function_exists( 'add_filter' ) ) {
 }
 
 // fire composer dependencies autoloader, making Abstract_Plugin class and other dependencies available
-include_once 'vendor/autoload.php';
+if ( ! class_exists( 'WPAZ_Plugin_Base\\V_2_5\\Abstract_Plugin' ) ) {
+    include_once  trailingslashit( dirname( __FILE__ ) )  . 'vendor/wordpress-phoenix/abstract-plugin-base/src/abstract-plugin.php';
+}
 
 // make plugin class available for run below
 include_once 'app/class-toolkit.php';
