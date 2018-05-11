1fbf8a945c576cf2aa77279dd07694a1772544f6
David Ryan <0aveRyan@users.noreply.github.com>
2018-05-11 14:31:01 -0700

    Update class-app.php

--- app/boilerplates/simple/app/admin/class-app.php
+++ app/boilerplates/simple/app/admin/class-app.php
@@ -14,32 +14,32 @@ namespace <%= PRIMARY_NAMESPACE %>\<%= SECONDARY_NAMESPACE %>\Admin;
 class App {
 
 	/**
-	 * Plugins class object installed directory on the server
+	 * Plugins class object installed directory on the server.
 	 *
-	 * @var string $installed_dir Installed server directory
+	 * @var string $installed_dir Installed server directory.
 	 */
 	public $installed_dir;
 
 	/**
-	 * Plugins URL for access to any static files or assets like css, js, or media
+	 * Plugins URL for access to any static files or assets like css, js, or media.
 	 *
-	 * @var string $installed_url Installed URL
+	 * @var string $installed_url Installed URL.
 	 */
 	public $installed_url;
 
 	/**
-	 * If plugin_data is built, this represents the version number defined the the main plugin file meta
+	 * If plugin_data is built, this represents the version number defined the the main plugin file meta.
 	 *
-	 * @var string $version Version
+	 * @var string $version Version.
 	 */
 	public $version;
 
 	/**
 	 * Add auth'd/admin functionality via new Class() instantiation, add_action() and add_filter() in this method.
 	 *
-	 * @param string $installed_dir Installed server directory
-	 * @param string $installed_url Installed URL
-	 * @param string $version       Version
+	 * @param string $installed_dir Installed server directory.
+	 * @param string $installed_url Installed URL.
+	 * @param string $version       Version.
 	 */
 	function __construct( $installed_dir, $installed_url, $version ) {
 		$this->installed_dir = $installed_dir;
